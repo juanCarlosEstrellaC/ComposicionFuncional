a09f90eaedc22945f75ddfceadf2e3ee47f5d1a4
Juan Estrella <jcestrella@uce.edu.ec>
2023-06-22 08:57:04 -0500

    borrador deber usando la función de composicion.

--- src/func/IComposicion.java
+++ src/func/IComposicion.java
@@ -13,4 +13,15 @@ public interface IComposicion {
 	static Function<Integer, Integer> composicion3(Function<Integer, Integer> f, Function<Integer, Integer> g) {
 		return x -> f.apply(g.apply(x));
 	}
+
+	// ************* EN CLASE: USO LA COMPOSICIÓN SIN PARÁMETROS EN EL MÉTODO, QUE ME DEVUELVE EL TRD DE 5Fs ******************
+	static <T> Function<Function<T, T>, Function<Function<T, T>, Function<T, T>>> composicion4() {
+		return fn -> (gn -> x -> gn.apply(fn.apply(x)));
+	}
+	
+	static <T, U, V> Function<Function<T, U>, Function<Function<U, V>, Function<T, V>>> composicion5() {
+		return fn -> (gn -> x -> gn.apply(fn.apply(x)));
+	}
+	
+	// 
 }
--- src/func/Main.java
+++ src/func/Main.java
@@ -1,6 +1,7 @@
 package func;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 
@@ -25,80 +26,88 @@ public class Main {
 //		System.out.println(c);
 //
 		// --------------------------  MODELIZACION DE COMPOSICIÓN DE FUNCIONES gof = g(f(x)) ---------------------
-//
-//		Function<Integer, Integer> f = x -> 3 * x;
-//		Function<Integer, Integer> g = x -> x * x;
-//		
-//		// 1. Usando la interface funcional Function:
-//			/* Así sería como normalmente he usado:
-//			 
-//			 	 F< I , I> sumarUno
-//		 		 F< Dominio, Rango> sumarUno
-//		 		 
-//				 Function<Integer, Integer> sumarUno = x -> x+1;
-//				 var v = sumarUno.apply(5);
-//				 System.out.println(v);
-//			
-//				Pero, en esta ocasión, el Function es de esta manera:
-//			    	
-//			    	F< F<I,I> , F<F<I,I>,F<I,I>>> compo
-//		 		    F< Dominio,       Rango     > compo
-//			 */
-//		
-//			Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>> compo = fn -> (gn -> x -> gn.apply(fn.apply(x)));
-//			Integer d = compo.apply(f).apply(g).apply(2);
-//			System.out.println(d);
-//
-//
-//		
-//		// USANDO INTERFACES:
-//		IComposicion iComposicion = () -> fn -> (gn -> x -> gn.apply(fn.apply(x)));
-//		var funcionCompuesta = iComposicion.composicion();
-//		var fog = funcionCompuesta.apply(f).apply(g).apply(2);
-//		System.out.println("fog = f(g(x)) = "+fog);
-//		
-//		var funcionCompuesta2 = IComposicion.composicion2();
-//		var fog2 = funcionCompuesta2.apply(f).apply(g).apply(2);
-//		System.out.println("fog2 = f(g(x)) = "+fog2);
-//		
-//		var funcionCompuesta3 = IComposicion.composicion3(g, f);
-//		var fog3 = funcionCompuesta3.apply(2);
-//		System.out.println("fog3 = f(g(x)) = "+fog3);
-		
-		// DEBER:
-		
-		String txt = "hola mundo java hola abc";
-//		String [] words = txt.split(" ");
-		
-		Function<String, String[]> f = cadena -> cadena.split(" ");
 
-		Function<String[], List<Par>> g = arregloStrings -> {
+		Function<Integer, Integer> f = x -> 3 * x;
+		Function<Integer, Integer> g = x -> x * x;
+		
+		// 1. Usando la interface funcional Function:
+			/* Así sería como normalmente he usado:
+			 
+			 	 F< I , I> sumarUno
+		 		 F< Dominio, Rango> sumarUno
+		 		 
+				 Function<Integer, Integer> sumarUno = x -> x+1;
+				 var v = sumarUno.apply(5);
+				 System.out.println(v);
 			
-			List<Par> listaPares = new ArrayList<>();
+				Pero, en esta ocasión, el Function es de esta manera:
+			    	
+			    	F< F<I,I> , F<F<I,I>,F<I,I>>> compo
+		 		    F< Dominio,       Rango     > compo
+			 */
+		
+			Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>> compo = fn -> (gn -> x -> gn.apply(fn.apply(x)));
+			Integer d = compo.apply(f).apply(g).apply(2);
+			System.out.println(d);
+
+
+		
+		// 2. USANDO INTERFACES:
+			IComposicion iComposicion = () -> fn -> (gn -> x -> gn.apply(fn.apply(x)));
+			var funcionCompuesta = iComposicion.composicion();
+			var fog = funcionCompuesta.apply(f).apply(g).apply(2);
+			System.out.println("fog = f(g(x)) = "+fog);
 			
-			for (String palabra : arregloStrings) {
-				int contador = 0;
-				for (String palabra2 : arregloStrings) {
-					if (palabra.equals(palabra2)) {
-						contador++;
+			var funcionCompuesta2 = IComposicion.composicion2();
+			var fog2 = funcionCompuesta2.apply(f).apply(g).apply(2);
+			System.out.println("fog2 = f(g(x)) = "+fog2);
+			
+			var funcionCompuesta3 = IComposicion.composicion3(g, f);
+			var fog3 = funcionCompuesta3.apply(2);
+			System.out.println("fog3 = f(g(x)) = "+fog3);
+			
+			
+			// ******************************* EN CLASE ********************************************
+			var funcionCompuesta4 = IComposicion.<Integer>composicion4();
+			var fog4 = funcionCompuesta4.apply(f).apply(g).apply(2);
+			System.out.println("fog4 = f(g(x)) = "+fog4);
+			
+			var funcionCompuesta5 = IComposicion.<Integer, Integer, Integer>composicion5();
+			var fog5 = funcionCompuesta5.apply(f).apply(g).apply(2);
+			System.out.println("fog5 = f(g(x)) = "+fog5);
+			
+			
+			// ********************************  DEBER *******************************************
+			String txt = "hola mundo java hola abc";
+			
+			Function<String, String[]> funcion1 = cadena -> cadena.split(" ");
+	
+			Function<String[], List<Par>> funcion2 = arregloStrings -> {
+				List<Par> listaPares = new ArrayList<>();
+				for (String palabra : arregloStrings) {
+					int contador = 0;
+					for (String palabra2 : arregloStrings) {
+						if (palabra.equals(palabra2)) {
+							contador++;
+						}
 					}
+					Par par = new Par(palabra, contador);
+					listaPares.add(par);
 				}
-				Par par = new Par(palabra, contador);
-				listaPares.add(par);
+				return listaPares;
+			};
+			
+			// Sin usar la funcion de composición:
+			List<Par> listaPares = funcion2.apply(funcion1.apply(txt));
+			for (Par par : listaPares) {
+				System.out.println(par);
 			}
-			return listaPares;
-		};
-
-		var a = f.apply(txt);
-//		for (String string : a) {	
-//			System.out.println(string);
-//		}
-		
-		var b = g.apply(a);
-		for (Par par : b) {
-			System.out.println(par);
+	
+			// Usando la función de composición:
+			List<Par> listaPares2 = IComposicion.<String, String[], List<Par>>composicion5().apply(funcion1).apply(funcion2).apply(txt);
+			for (Par par : listaPares2) {
+				System.out.println(par);
+			}
 		}
-
-	}
 
 }
